6948d04fca4b334ccf10f9a4e43e28c99825ff09
Abzal Suan <78753207+hj-abzal@users.noreply.github.com>
2022-09-09 13:41:02 +0600

    Update config.ts

--- src/lb-1-main/config.ts
+++ src/lb-1-main/config.ts
@@ -4,5 +4,5 @@ const MONGO_DB_URL = process.env.MONGO_DB_URL || 'cluster0.s4ai6.mongodb.net'; /
 
 export const MongoDBUris = `mongodb+srv://${USER_NAME}:${PASSWORD}@${MONGO_DB_URL}?retryWrites=true&w=majority`;
 export const DEV_VERSION = false;
-export const PORT = 7542;
+export const PORT = 8080;
 
